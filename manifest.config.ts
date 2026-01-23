c65bde2d0e5a0745c4cd20d7cd6a793264bad12d
Enzo <enzonbarata@outlook.com>
2026-01-23 02:18:30 -0300

    chore: manifest

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,32 +1,35 @@
 import { defineManifest } from '@crxjs/vite-plugin';
 import pkg from './package.json';
 
+const icons = {
+	16: 'public/logo16.png',
+	32: 'public/logo32.png',
+	48: 'public/logo48.png',
+	128: 'public/logo128.png',
+	512: 'public/logo512.png'
+};
+
 export default defineManifest({
 	manifest_version: 3,
-	name: pkg.name,
+	name: 'Quick Reply Meet',
 	version: pkg.version,
-	icons: {
-		48: 'public/logo.png'
-	},
+	description: '__MSG_extensionDescription__',
+	icons,
 	default_locale: 'pt_BR',
+	homepage_url: 'https://quickreplymeet.enzon19.com',
+	options_page: 'src/options/index.html',
+	permissions: [
+		'storage'
+		// "contentSettings",
+	],
 	action: {
-		default_icon: {
-			48: 'public/logo.png'
-		},
+		default_icon: icons,
 		default_popup: 'src/popup/index.html'
-	},
+	}
 	// content_scripts: [
 	//   {
 	//     js: ["src/content/main.ts"],
 	//     matches: ["https://*/*"],
 	//   },
-	// ],
-	permissions: [
-		'storage'
-		// 'sidePanel',
-		// "contentSettings",
-	]
-	// side_panel: {
-	//   default_path: 'src/sidepanel/index.html',
-	// },
+	// ]
 });
