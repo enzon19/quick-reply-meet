71f487acc235aa5a3d0569216c083fa170c47b55
Enzo <enzonbarata@outlook.com>
2026-01-23 01:12:53 -0300

    feat: buttons position and roundness settings

--- src/popup/pages/Settings.svelte
+++ src/popup/pages/Settings.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import { ListItem, Divider, Icon, Button } from 'm3-svelte';
-	import { icons as materialSymbols } from '@iconify-json/material-symbols/icons.json';
+	import { ListItem, Divider, Icon, Button, Slider, RadioAnim1 } from 'm3-svelte';
 	import AppBar from '@/components/AppBar.svelte';
+	import ChatIllustration from '@/components/ChatIllustration.svelte';
 
+	import { icons as materialSymbols } from '@iconify-json/material-symbols/icons.json';
+	import { onMount } from 'svelte';
 	const roundnessIcon = materialSymbols['rounded-corner'];
 	const positionIcon = materialSymbols['position-bottom-left'];
 	const infoIcon = materialSymbols['info'];
@@ -14,6 +16,31 @@
 	}: {
 		page: SettingsSubpages;
 	} = $props();
+
+	let borderRadius: number | undefined = $state();
+	let buttonsPosition: 'top' | 'bottom' | undefined = $state();
+
+	onMount(async () => {
+		const { borderRadius: borderRadiusFromSync } = await chrome.storage.sync.get('borderRadius');
+		const { buttonsPosition: buttonsPositionFromSync } =
+			await chrome.storage.sync.get('buttonsPosition');
+
+		if (borderRadiusFromSync != null) {
+			borderRadius = Number(borderRadiusFromSync);
+		} else {
+			borderRadius = 20;
+		}
+
+		if (buttonsPositionFromSync && ['top', 'bottom'].includes(buttonsPositionFromSync.toString()))
+			buttonsPosition = buttonsPositionFromSync as 'top' | 'bottom';
+	});
+
+	$effect(() => {
+		if (borderRadius != null) chrome.storage.sync.set({ borderRadius });
+	});
+	$effect(() => {
+		if (buttonsPosition) chrome.storage.sync.set({ buttonsPosition });
+	});
 </script>
 
 {#if page}
@@ -23,9 +50,29 @@
 {/if}
 
 {#if page == 'buttonsRoundness'}
-	buttonsRoundness
+	<ChatIllustration {borderRadius} {buttonsPosition} />
+	<div style="margin-top: 16px">
+		{#if borderRadius != null}
+			<Slider bind:value={borderRadius} endStops={false} max={25} />
+		{/if}
+	</div>
 {:else if page == 'buttonsPosition'}
-	buttonsPosition
+	<ChatIllustration {borderRadius} {buttonsPosition} />
+
+	<ListItem label headline={chrome.i18n.getMessage('topButtonsPosition')} style="margin-top: 16px">
+		{#snippet leading()}
+			<RadioAnim1>
+				<input type="radio" name="buttonsPosition" value="top" bind:group={buttonsPosition} />
+			</RadioAnim1>
+		{/snippet}
+	</ListItem>
+	<ListItem label headline={chrome.i18n.getMessage('bottomButtonsPosition')}>
+		{#snippet leading()}
+			<RadioAnim1>
+				<input type="radio" name="buttonsPosition" value="bottom" bind:group={buttonsPosition} />
+			</RadioAnim1>
+		{/snippet}
+	</ListItem>
 {:else if page == 'about'}
 	<img src="/logo512.png" class="logo" alt="Quick Reply Meet Logo" />
 	<h1>Quick Reply Meet</h1>
@@ -90,5 +137,21 @@
 		flex-direction: row;
 		gap: 5px;
 		margin: 18px auto 0px auto;
+	}
+
+	label {
+		display: inline-flex;
+		font-size: 0.875rem;
+		gap: 1.5rem;
+	}
+
+	.radios {
+		display: flex;
+		flex-direction: column;
+		gap: 1.5rem;
+	}
+
+	:global(.m3-container input[type='range'] ~ .value) {
+		inset-inline-start: clamp(0rem, var(--handle-center), calc(100% - 0.5rem));
 	}
 </style>
