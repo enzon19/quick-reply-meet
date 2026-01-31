e6d0cbf5f93138de293f85fcd54797d6477ae26b
Enzo <enzonbarata@outlook.com>
2026-01-31 15:31:23 -0300

    feat: i18n

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,15 @@
+import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n';
+
+// import en from './en.json';
+// addMessages('en', en);
+
+import ptBR from './pt-BR.json';
+addMessages('pt-BR', ptBR);
+
+// import es from './es.json';
+// addMessages('es', es);
+
+init({
+	fallbackLocale: 'pt-BR',
+	initialLocale: getLocaleFromNavigator()
+});
