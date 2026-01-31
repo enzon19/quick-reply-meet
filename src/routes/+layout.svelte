798e9a3193855e7a0059db2360afec29a7ffdfe7
Enzo <enzonbarata@outlook.com>
2026-01-31 15:32:21 -0300

    feat: header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import '$lib/i18n/index';
+	import { _ } from 'svelte-i18n';
+	import { Button } from 'm3-svelte';
+	import favicon from '$lib/assets/favicon.ico';
+	import icon from '$lib/assets/icon-512.png';
 
 	let { children } = $props();
 </script>
@@ -9,4 +13,52 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header>
+	<div class="container header">
+		<div class="logo">
+			<img src={icon} alt="Quick Reply Meet Logo" />
+			<h1>Quick Reply Meet</h1>
+		</div>
+		<Button>{$_('install')}</Button>
+	</div>
+</header>
+
 {@render children()}
+
+<style>
+	header {
+		background-color: var(--m3c-surface-container-highest);
+		margin: -1rem -1rem 0 -1rem;
+		padding: 0.8rem;
+	}
+
+	.header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.logo {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.logo > img {
+		height: 3.25rem;
+	}
+
+	.logo > h1 {
+		display: none;
+		margin: 0;
+		font-weight: 600;
+		font-size: 1.5rem;
+	}
+
+	
+	@media (min-width: 480px) {
+		.logo > h1 {
+			display: block;
+		}
+	}
+</style>
