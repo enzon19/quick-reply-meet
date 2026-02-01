b55f31f7ed08691f7cca759765a69637ac3ed615
Enzo <enzonbarata@outlook.com>
2026-02-01 16:00:24 -0300

    feat: en and es locales

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,15 +1,15 @@
 import { addMessages, getLocaleFromNavigator, init } from 'svelte-i18n';
 
-// import en from './en.json';
-// addMessages('en', en);
+import en from './en.json';
+addMessages('en', en);
 
 import ptBR from './pt-BR.json';
 addMessages('pt-BR', ptBR);
 
-// import es from './es.json';
-// addMessages('es', es);
+import es from './es.json';
+addMessages('es', es);
 
 init({
-	fallbackLocale: 'pt-BR',
+	fallbackLocale: 'en',
 	initialLocale: getLocaleFromNavigator()
 });
