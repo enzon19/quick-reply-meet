224adf10ea2b060efabd717b2152abf47a5ae912
Enzo <enzonbarata@outlook.com>
2026-02-01 20:59:29 -0300

    build website

---
+++ _app/immutable/entry/start.1hlhOc0g.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DRu2L8vi.js";export{o as load_css,r as start};
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { functionsMixins } from 'vite-plugin-functions-mixins';
-
-export default defineConfig({
-	plugins: [sveltekit(), functionsMixins({ deps: ['m3-svelte'] })]
-});
